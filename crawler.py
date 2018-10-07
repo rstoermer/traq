c6f1581a13fd28b6688f88b5a2716f4ec47dda7f
acc@strmr.de <acc@strmr.de>
2018-10-07 19:02:27 +0200

    Added BTC

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ from fints.client import FinTS3PinTanClient
 import time
 from datetime import datetime
 from functions import *
+import requests
+import json
 
 def main():
     config = load_config('config.json')
@@ -9,14 +11,32 @@ def main():
     timestamp = datetime.today().strftime('%d.%m.%Y - %H:%M')
 
     for entry in config['accounts_nr'].items():
+        #Mit Konto verbinden
         f = FinTS3PinTanClient(entry[1]['blz'], entry[1]['nr'], entry[1]['pass'], 'https://fints.ing-diba.de/fints/')
+        
+        #Konten abrufen
         accountList = f.get_sepa_accounts()
         for account in accountList:
+            #Wenn das Konto ein Depot ist:
             if account.accountnumber in config['depots_nr']:
                 accountholdings = f.get_holdings(account)
+                #Für jede Position die aktuellen Werte abrufen
                 for accountholding in accountholdings:
                     save_db(timestamp, entry[1]['name'], accountholding.ISIN, accountholding.name, accountholding.market_value, accountholding.value_symbol, accountholding.pieces, accountholding.total_value, accountholding.acquisitionprice)
                     print(timestamp)
+
+    #Add Bitcoin holdings
+    lastBTCPrice = 0
+    BTCAmount = float(config['BTCAmount'][0])
+    try:
+        r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json', timeout=10)
+        BTCPrice = json.loads(r.text)['bpi']['EUR']['rate_float']
+        lastBTCPrice = BTCPrice
+        save_db(timestamp, 'Gemeinschaftskonto', 'BTC', 'BTC', BTCPrice, 'EUR', BTCAmount, BTCPrice * BTCAmount, 4400)
+
+    except requests.exceptions.RequestException as e:
+        save_db(timestamp, 'Gemeinschaftskonto', 'BTC', 'BTC', lastBTCPrice, 'EUR', BTCAmount, lastBTCPrice * BTCAmount, 4400)
+        print(e)
 
 if __name__ == '__main__':
     while True:
