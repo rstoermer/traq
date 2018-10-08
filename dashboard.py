6eb03e6ff3c6d1e54fdb5a60882f3b6025d43ae0
acc@strmr.de <acc@strmr.de>
2018-10-08 18:35:47 +0200

    Annabelle was here

--- dashboard.py
+++ dashboard.py
@@ -9,31 +9,24 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#Load the current depot
-con = db_connect()
-depot = pd.read_sql_query("SELECT * FROM accountholdings", con)
-con.close()
+def serve_layout():
+    #Load the current depot
+    con = db_connect()
+    depot = pd.read_sql_query("SELECT * FROM accountholdings", con)
+    con.close()
 
-#transform the dataframe
-depot['position'] = depot['account'] + " - " + depot['name']
-depot = depot.pivot_table(index='date', columns='position', values='total_value')
+    #transform the dataframe
+    depot['position'] = depot['account'] + " - " + depot['name']
+    depot = depot.pivot_table(index='date', columns='position', values='total_value')
 
-traces = []
-for position, values in depot.iteritems():
-    traces.append(go.Scatter(x=values.index, y=values.values, name=position, stackgroup='A'))
+    traces = []
+    for position, values in depot.iteritems():
+        traces.append(go.Scatter(x=values.index, y=values.values, name=position, stackgroup='A'))
+
+    return html.Div([dcc.Graph(id='Overview', style={'height': '100vh'}, figure={'data': traces,'layout': go.Layout(hovermode='closest')})])
 
 #Generate the Dash App
-app.layout = html.Div([
-    dcc.Graph(
-        id='Overview',
-        figure={
-            'data': traces,
-            'layout': go.Layout(
-                hovermode='closest'
-            )
-        }
-    )
-])
+app.layout = serve_layout
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='192.168.1.3', port=8050)
